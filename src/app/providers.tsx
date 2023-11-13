f177a96c36054b82312a809c9dee6e0e1cbe466f
Kalmovic <joseph.ext@rallyrd.com>
2023-11-13 01:07:09 -0300

    add providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,21 @@
-'use client'
+"use client";
 
-import { ConnectKitProvider } from 'connectkit'
-import * as React from 'react'
-import { WagmiConfig } from 'wagmi'
+import { ConnectKitProvider } from "connectkit";
+import * as React from "react";
+import { WagmiConfig } from "wagmi";
+import { Toaster } from "react-hot-toast";
 
-import { config } from '../wagmi'
+import { config } from "../wagmi";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
   return (
     <WagmiConfig config={config}>
-      <ConnectKitProvider>{mounted && children}</ConnectKitProvider>
+      <ConnectKitProvider>
+        <Toaster />
+        {mounted && children}
+      </ConnectKitProvider>
     </WagmiConfig>
-  )
+  );
 }
