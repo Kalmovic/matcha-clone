7141db267af8c87d2c805a16c8019109897e6489
Kalmovic <joseph.ext@rallyrd.com>
2023-11-13 01:03:32 -0300

    setting up

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false }
-    return config
+  images: {
+    domains: ["raw.githubusercontent.com"],
   },
-}
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
