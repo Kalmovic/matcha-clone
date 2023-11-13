40b084a1357df75f425999efb9da509fed6212e9
Kalmovic <joseph.ext@rallyrd.com>
2023-11-13 02:12:20 -0300

    add skeleton componnent from shadcb

---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,15 @@
+import { cn } from "../../libs/libs";
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
